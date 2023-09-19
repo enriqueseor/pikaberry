550b0003e5fb8b1033353bd90d667377aed2c323
enriqueseor <enriqueseor@gmail.com>
2023-09-19 16:04:32 +0100

    added rock model class

---
+++ app/src/main/java/cat/teknos/berry/model/Rock.java
@@ -0,0 +1,36 @@
+package cat.teknos.berry.model;
+
+public class Rock {
+    private int x, y;
+    private int type;
+
+    public Rock(int x, int y, int type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+}
